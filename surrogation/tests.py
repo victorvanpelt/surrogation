993aebc25937fa4767ffc0abe5a74ebf0fc74e7b
Victor van Pelt <victorvanpelt@protonmail.com>
2024-04-23 16:12:26 +0200

    Updated to no-self and improved

--- surrogation/tests.py
+++ surrogation/tests.py
@@ -1,11 +1,11 @@
-from otree.api import Currency as c, currency_range
-from . import pages
-from ._builtin import Bot
-from .models import Constants
+from otree.api import Currency as cu, currency_range
+from . import *
+from otree.api import Bot
+
 
 
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (pages.MyPage)
-        yield (pages.Results)
+        yield (MyPage)
+        yield (Results)
